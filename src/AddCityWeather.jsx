fbb36f7e2f1586a62f6fb1defc4ba4d001ead779
NofiK <slavikgrechukh@gmail.com>
2022-06-04 20:28:43 +0300

    Mobile size

--- src/AddCityWeather.jsx
+++ src/AddCityWeather.jsx
@@ -1,28 +1,40 @@
-import React, {useState} from 'react'
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
-const AddCityWeather = ({addCity}) => {
-    const [city, setCity] = useState("");
+const AddCityWeather = ({ addCity }) => {
+  const [city, setCity] = useState("");
 
-    async function fetchWeather(){
-      const response = await axios.get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=ce76ddc943bc1e29df66a0a394e5334c&units=metric")
-      let newCity = {
-        id: response.data.id,
-        city: response.data.name, //name
-        country: response.data.sys.country, //sys.country
-        celsius: Math.round(response.data.main.temp), //main.temp
-        weather: response.data.weather[0].description, //weather[0].description
-        icon: response.data.weather[0].icon //weather[0].icon
-      }
-      addCity(newCity);
-      setCity("");
-    }
+  async function fetchWeather() {
+    const response = await axios.get(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&appid=ce76ddc943bc1e29df66a0a394e5334c&units=metric"
+    );
+    let newCity = {
+      id: response.data.id,
+      city: response.data.name, //name
+      country: response.data.sys.country, //sys.country
+      celsius: Math.round(response.data.main.temp), //main.temp
+      weather: response.data.weather[0].description, //weather[0].description
+      icon: response.data.weather[0].icon, //weather[0].icon
+    };
+    addCity(newCity);
+    setCity("");
+  }
   return (
     <div>
-      <input placeholder='Search for a city' className='addInput' value={city} onChange={e=>setCity(e.target.value)} type="text" />
-      <button className='addBtn' onClick={fetchWeather}>Submit</button>
+      <input
+        placeholder="Search for a city"
+        className="addInput"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        type="text"
+      />
+      <button className="addBtn" onClick={fetchWeather}>
+        Submit
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default AddCityWeather
+export default AddCityWeather;
--- src/WeatherList.jsx
+++ src/WeatherList.jsx
@@ -1,32 +1,32 @@
-import React, {useState} from 'react'
-import WeatherItem from './WeatherItem'
-import AddCityWeather from './AddCityWeather'
+import React, { useState } from "react";
+import WeatherItem from "./WeatherItem";
+import AddCityWeather from "./AddCityWeather";
 
 const WeatherList = () => {
-  
   const [cities, setCities] = useState([
-  {
+    {
       id: "qgauierhcng7anin4egnncmx",
       city: "London", //name
       country: "UK", //sys.country
-      celsius: 7, //main.temp
+      celsius: 14, //main.temp
       weather: "Rainy", //weather[0].description
-      icon: "03d" //weather[0].icon
-  }])
- function addNewCity(newCity){
-  setCities([...cities, newCity])
- }
+      icon: "03d", //weather[0].icon
+    },
+  ]);
+  function addNewCity(newCity) {
+    setCities([...cities, newCity]);
+  }
 
   return (
     <div>
       <AddCityWeather addCity={addNewCity} />
-      <div className='weatherList'>
-      {cities.map(item=>
-        <WeatherItem city={item} key={item.id} />
-      )}
-       </div>
+      <div className="weatherList">
+        {cities.map((item) => (
+          <WeatherItem city={item} key={item.id} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WeatherList
+export default WeatherList;
